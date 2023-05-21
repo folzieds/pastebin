0d9f7fd876334d04498bc3c3dc5cd3091a941c67
folzieds <folzieds@gmail.com>
2023-05-21 22:38:14 +0100

    Added tests

---
+++ src/test/java/com/carbon/pastebin/controllers/ContentControllerTest.java
@@ -0,0 +1,16 @@
+package com.carbon.pastebin.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContentControllerTest {
+
+    @Test
+    void createSharedContent() {
+    }
+
+    @Test
+    void retrieveSharedContent() {
+    }
+}
---
+++ src/test/java/com/carbon/pastebin/services/ContentServiceTest.java
@@ -0,0 +1,16 @@
+package com.carbon.pastebin.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContentServiceTest {
+
+    @Test
+    void createContent() {
+    }
+
+    @Test
+    void getSharedContent() {
+    }
+}
